fdbcec6a082556c7dadf30e866d1af747204b67c
Aashika Lilaramani <alila2@uic.edu>
2025-10-30 15:36:03 -0500

    first push!

---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        question: "Which of these dietary restrictions apply to you?",
+        option: ["Vegan", "Vegetarian", "Gluten Free", "None"],
+    },
+    {
+        question: "How active are you on a weekly basis?",
+        option: ["Not active", "A few days of light exercise", "A few days of moderate exercise", "Many days of intense exercise"],
+    },
+    {
+        question: "What is your main health goal?",
+        option: ["Lose weight", "Gain muscle", " Maintain weight", "Improve overall health"],
+    },
+];
